d2cd57d7794810100f814667057bec820cf6cc85
hervay.viky@gmail.com <hervay.viky@gmail.com>
2022-08-11 19:25:48 +0200

    eslint fix

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -1,48 +1,50 @@
-import HomePage from '../pages/HomePage';
-class LoginPage{
-    //Selectors
-    public get emailInput(){
+/* eslint-disable import/no-unresolved */
+import HomePage from './HomePage';
+
+class LoginPage {
+    // Selectors
+    public get emailInput() {
         return $('#username');
     }
 
-    public get passInput(){
+    public get passInput() {
         return $('#password');
     }
 
-    public get loginBtn(){
+    public get loginBtn() {
         return $('#loginButton');
     }
 
-    public get signOutBtn(){
+    public get signOutBtn() {
         return $('.button-as-link');
     }
 
-    public get errorMessage(){
+    public get errorMessage() {
         return $('#loginContainer > #loginForm > .error');
     }
 
-    //Functions
+    // Functions
 
-    public async login(email: string, pass: string){
+    public async login(email: string, pass: string) {
         await this.emailInput.setValue(email);
         await this.passInput.setValue(pass);
         await this.loginBtn.click();
     }
 
-    public async signOut(){
+    public async signOut() {
         await this.signOutBtn.click();
         expect(HomePage.signInBtn).toBeDisplayed();
     }
 
-    public async loginWithInvalidEmailFormat(){
+    public async loginWithInvalidEmailFormat() {
         await expect(this.errorMessage).toHaveTextContaining('is not a valid username.');
     }
 
-    public async loginWithInvalidEmail(){
+    public async loginWithInvalidEmail() {
         await expect(this.errorMessage).toHaveTextContaining('Wrong username or password');
     }
 
-    public async loginWithInvalidPass(){
+    public async loginWithInvalidPass() {
         await expect(this.errorMessage).toHaveTextContaining('Wrong username or password');
     }
 }
